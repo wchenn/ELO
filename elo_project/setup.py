96ee818a6dc20c923cd805ada24250ab8d897b52
William Chen <wc1321@uw.edu>
2023-07-12 21:17:53 -0700

    first commit

---
+++ elo_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='idk what im doing lol',
+    author='William',
+    license='MIT',
+)
